e0ee73dc9ca81b3d56acc5979a729e9bf89f7850
Thomas Rausch <thomas@rauschig.org>
2020-07-02 19:27:11 +0200

    Add multi-experiment selector

--- galileojp/widgets.py
+++ galileojp/widgets.py
@@ -4,10 +4,16 @@ from galileojp.frames import ExperimentFrameGateway
 
 
 def experiment_dropdown(fgw: ExperimentFrameGateway):
-    options = fgw.experiments()['EXP_ID']
+    options = [(f'{row[1]}: {row[0]}', row[1]) for row in fgw.experiments()[['NAME', 'EXP_ID']].values]
 
     return widgets.Dropdown(
         options=options,
         description='Experiment:',
         disabled=False,
     )
+
+
+def experiment_selector(fgw: ExperimentFrameGateway):
+    options = [(f'{row[1]}: {row[0]}', row[1]) for row in fgw.experiments()[['NAME', 'EXP_ID']].values]
+    exp_selector = widgets.SelectMultiple(options=options, description='Experiment:')
+    return exp_selector
