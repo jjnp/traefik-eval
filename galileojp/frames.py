8c07562c42db746a4a690de7c6022a81fba3de94
Thomas Rausch <thomas@rauschig.org>
2020-07-02 16:39:09 +0200

    Restructure frame gateway api and add configuration options

--- galileojp/frames.py
+++ galileojp/frames.py
@@ -1,5 +1,13 @@
 import pandas as pd
+from galileodb.factory import create_experiment_database_from_env
 from galileodb.sql.adapter import ExperimentSQLDatabase
+
+from galileojp import env
+
+
+def to_idlist(exp_ids):
+    idlist = ', '.join([f'"{i}"' for i in exp_ids])
+    return idlist
 
 
 class ExperimentFrameGateway:
@@ -7,19 +15,45 @@ class ExperimentFrameGateway:
     def __init__(self, edb: ExperimentSQLDatabase) -> None:
         super().__init__()
         self.edb = edb
-        self.exp_id = None
 
-    def load_experiment(self, exp_id) -> pd.DataFrame:
-        exp = self.edb.get_experiment(exp_id)
-        if not exp:
-            raise ValueError('no such experiment')
+    @property
+    def _con(self):
+        return self.edb.db.connection
 
-        self.exp_id = exp_id
+    def experiments(self) -> pd.DataFrame:
+        return pd.read_sql(f'SELECT * FROM experiments', con=self._con)
 
-        return pd.read_sql(f'SELECT * FROM experiments WHERE exp_id = "{exp_id}"', con=self.edb.db.connection)
+    def nodeinfo(self, *exp_ids):
+        if not exp_ids:
+            raise ValueError
 
-    def events(self):
-        return pd.read_sql(f'SELECT * FROM events WHERE exp_id = "{self.exp_id}"', con=self.edb.db.connection)
+        idlist = to_idlist(exp_ids)
 
-    def telemetry(self):
-        return pd.read_sql(f'SELECT * FROM telemetry WHERE exp_id = "{self.exp_id}"', con=self.edb.db.connection)
+        df = pd.read_sql(f'SELECT * FROM nodeinfo WHERE exp_id in ({idlist})', con=self._con)
+        df.index = pd.DatetimeIndex(pd.to_datetime(df['TIMESTAMP'], unit='s'))
+        return df
+
+    def events(self, *exp_ids):
+        if not exp_ids:
+            raise ValueError
+
+        idlist = to_idlist(exp_ids)
+
+        df = pd.read_sql(f'SELECT * FROM events WHERE exp_id in ({idlist})', con=self._con)
+        df.index = pd.DatetimeIndex(pd.to_datetime(df['TIMESTAMP'], unit='s'))
+        return df
+
+    def telemetry(self, *exp_ids) -> pd.DataFrame:
+        if not exp_ids:
+            raise ValueError
+
+        idlist = to_idlist(exp_ids)
+
+        df = pd.read_sql(f'SELECT * FROM telemetry WHERE exp_id in ({idlist})', con=self._con)
+        df.index = pd.DatetimeIndex(pd.to_datetime(df['TIMESTAMP'], unit='s'))
+        return df
+
+    @staticmethod
+    def from_env() -> 'ExperimentFrameGateway':
+        env.load()
+        return ExperimentFrameGateway(create_experiment_database_from_env())
