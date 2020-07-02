d500dfa4bb0ef83ff111a7db26a701678654a05a
Thomas Rausch <thomas@rauschig.org>
2020-07-02 19:53:06 +0200

    Set default width of widgets to 100%

--- galileojp/widgets.py
+++ galileojp/widgets.py
@@ -3,17 +3,24 @@ import ipywidgets as widgets
 from galileojp.frames import ExperimentFrameGateway
 
 
-def experiment_dropdown(fgw: ExperimentFrameGateway):
+def experiment_dropdown(fgw: ExperimentFrameGateway, **kwargs):
     options = [(f'{row[1]}: {row[0]}', row[1]) for row in fgw.experiments()[['NAME', 'EXP_ID']].values]
 
     return widgets.Dropdown(
         options=options,
         description='Experiment:',
         disabled=False,
+        layout=widgets.Layout(width='100%'),
+        **kwargs
     )
 
 
-def experiment_selector(fgw: ExperimentFrameGateway):
+def experiment_selector(fgw: ExperimentFrameGateway, **kwargs):
     options = [(f'{row[1]}: {row[0]}', row[1]) for row in fgw.experiments()[['NAME', 'EXP_ID']].values]
-    exp_selector = widgets.SelectMultiple(options=options, description='Experiment:')
-    return exp_selector
+
+    return widgets.SelectMultiple(
+        options=options,
+        description='Experiment:',
+        layout=widgets.Layout(width='100%'),
+        **kwargs
+    )
