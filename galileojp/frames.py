4d5f4293192567b7297db14ef6bd57829edde849
Jacob Palecek <jacob.palecek@outlook.com>
2021-05-08 23:38:01 +0200

    add more detailed visualisations, and experiment data

--- galileojp/frames.py
+++ galileojp/frames.py
@@ -1,5 +1,7 @@
+import collections
+
 import pandas as pd
-from galileodb.factory import create_experiment_database_from_env
+from galileodb.factory import create_experiment_database_from_env, create_experiment_database
 from galileodb.sql.adapter import ExperimentSQLDatabase
 
 from galileojp import env
@@ -52,7 +54,6 @@ class ExperimentFrameGateway:
 
         idlist = to_idlist(exp_ids)
         db_str = "SELECT j1.rid as rid, j1.sent as sent, j1.done as done, round((j1.done - j1.sent) * 1000) as rtt, j1.node as node, j1.cpu as cpu, j1.cputs as cputs, j2.ram as ram, j2.ramts as ramts, j1.cputs - j2.ramts as metric_ts_delta FROM (SELECT tr.EXP_ID as exp_id, tr.REQUEST_ID as rid, tr.SENT as sent, tr.DONE as done, te.VALUE as cpu, te.TIMESTAMP as cputs, te.NODE as node FROM traces tr LEFT OUTER JOIN telemetry te ON abs(tr.DONE - te.TIMESTAMP) < 0.5 AND te.METRIC = 'cpu' AND te.EXP_ID = tr.EXP_ID WHERE te.exp_id  IN ({idlist})) j1 JOIN (SELECT tr.REQUEST_ID as rid, tr.SENT as sent, tr.DONE as done, te.VALUE as ram, te.TIMESTAMP as ramts, te.NODE as node FROM traces tr LEFT OUTER JOIN telemetry te ON abs(tr.DONE - te.TIMESTAMP) < 0.5 AND te.METRIC = 'ram' AND te.EXP_ID = tr.EXP_ID WHERE te.exp_id  IN ({idlist})) j2 ON j1.rid = j2.rid AND j1.node = j2.node WHERE j1.exp_id  IN ({idlist})".format(idlist=idlist)
-        print(db_str)
         df = pd.read_sql(db_str, con=self._con)
         # df = pd.read_sql(f'SELECT * FROM traces trac WHERE trac.exp_id in ({idlist}) JOIN telemetry tel ON abs(trac.CREATED - tel.TIMESTAMP) < 1 WHERE tel.EXP_ID in ({idlist})', con=self._con)
         # df['RTT'] = df['DONE'] - df['SENT']
@@ -85,3 +86,11 @@ class ExperimentFrameGateway:
     def from_env() -> 'ExperimentFrameGateway':
         env.load()
         return ExperimentFrameGateway(create_experiment_database_from_env())
+
+    @staticmethod
+    def from_file(filename) -> 'ExperimentFrameGateway':
+        artificial_environment = {}
+        artificial_environment.setdefault("galileo_expdb_sqlite_path", filename)
+        artificial_environment.setdefault("galileo_expdb_driver", "sqlite")
+        return ExperimentFrameGateway(create_experiment_database('sqlite', env=artificial_environment))
+
--- galileojp/util.py
+++ galileojp/util.py
@@ -4,3 +4,7 @@ import numpy as np
 
 def fill_sparse_frame(df: pd.DataFrame, min_datetime: datetime, max_datetime: datetime, interval):
     pass
+
+
+def kek_test():
+    return 'hello world and something elese'
